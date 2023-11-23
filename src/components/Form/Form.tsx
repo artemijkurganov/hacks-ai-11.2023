ccd2ad1211647af8b93ee95944f8595a09a4ffb0
Artemii Kurganov <kurganov@skbkontur.ru>
2023-11-24 00:29:27 +0500

    remove mui

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,13 @@
-import { TextField } from "@mui/material";
 import styles from "./Form.module.css";
 import { model } from "../../../model.ts";
-import { Unstable_NumberInput as NumberInput } from "@mui/base/Unstable_NumberInput";
 
 export const Form = () => {
-  model;
   return (
     <div className={styles.container}>
       <h2>Образец формы</h2>
       <div className={styles.inputFields}>
         {model.map((x) => {
-          return x.inputType === "string" ? <TextField /> : <NumberInput />;
+          return x.inputType === "string" ? <div /> : <div />;
         })}
       </div>
     </div>
