b79ec16fc4ba519df39b6fdacbe827c14691e62a
Artemii Kurganov <kurganov@skbkontur.ru>
2023-11-25 16:00:09 +0500

    new script

--- script/calculation_of_payments_old.py
+++ script/calculation_of_payments_old.py
@@ -5,6 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from catboost import CatBoostRegressor
 import numpy as np
 warnings.filterwarnings('ignore')
+import os
 
 
 # Данные о постройках по кварталам
@@ -30,21 +31,28 @@ def process_quartal_data(df):
     df['Техническое состояние'] = df['Техническое состояние'].replace(replace_ts)
     return df
 
+def get2020xls(index):
+    return os.path.join('script', 'train_data', 'Lists_ON', 'List ON_2020', f'{index} quarter 2020.xls')
 
-quartal_1_2020 = pd.read_excel(r'train_data\Lists_ON\List ON_2020\1 quarter 2020.xls', skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
-quartal_2_2020 = pd.read_excel(r'train_data\Lists_ON\List ON_2020\2 quarter 2020.xls', skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
-quartal_3_2020 = pd.read_excel(r'train_data\Lists_ON\List ON_2020\3 quarter 2020.xls', skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
-quartal_4_2020 = pd.read_excel(r'train_data\Lists_ON\List ON_2020\4 quarter 2020.xls', skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
+def get2021xls(index):
+    return os.path.join('script', 'train_data', 'Lists_ON', 'List ON_2021', f'{index} quarter 2021.xls')
+
+
+
+quartal_1_2020 = pd.read_excel(get2020xls(1), skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
+quartal_2_2020 = pd.read_excel(get2020xls(2), skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
+quartal_3_2020 = pd.read_excel(get2020xls(3), skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
+quartal_4_2020 = pd.read_excel(get2020xls(4), skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
 
 quartal_1_2020 = process_quartal_data(quartal_1_2020)
 quartal_2_2020 = process_quartal_data(quartal_2_2020)
 quartal_3_2020 = process_quartal_data(quartal_3_2020)
 quartal_4_2020 = process_quartal_data(quartal_4_2020)
 
-quartal_1_2021 = pd.read_excel(r'train_data\Lists_ON\List ON_2021\1 quarter 2021.xls', skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
-quartal_2_2021 = pd.read_excel(r'train_data\Lists_ON\List ON_2021\2 quarter 2021.xls', skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
-quartal_3_2021 = pd.read_excel(r'train_data\Lists_ON\List ON_2021\3 quarter 2021.xls', skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
-quartal_4_2021 = pd.read_excel(r'train_data\Lists_ON\List ON_2021\4 quarter 2021.xls', skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
+quartal_1_2021 = pd.read_excel(get2021xls(1), skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
+quartal_2_2021 = pd.read_excel(get2021xls(2), skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
+quartal_3_2021 = pd.read_excel(get2021xls(3), skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
+quartal_4_2021 = pd.read_excel(get2021xls(4), skiprows=8, parse_dates=[4]).drop([3, 4, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 22, 24], axis=1)
 
 quartal_1_2021 = process_quartal_data(quartal_1_2021)
 quartal_2_2021 = process_quartal_data(quartal_2_2021)
@@ -53,13 +61,16 @@ quartal_4_2021 = process_quartal_data(quartal_4_2021)
 
 # Данные о температуре
 
-temperature_data = pd.read_excel('temperature_data.xlsx')
+temperature_data = pd.read_excel(os.path.join("script", "temperature_data.xls"))
 temperature_data.rename(columns={'Дата проведения': 'Месяц'}, inplace=True)
 
 # Данные о платежах
 
-exp_2020 = pd.read_excel(r'2020_razmech.xlsx', skiprows=9, parse_dates=['Дата проведения']).drop(['Unnamed: 0', 'Смета', 'Начальный остаток', 'Конечный остаток', 'Код статьи', 'Код вида расходов', 'Unnamed: 12'], axis=1)
-exp_2021 = pd.read_excel(r'2021_razmech.xlsx', skiprows=9, parse_dates=['Дата проведения']).drop(['Unnamed: 0', 'Смета', 'Начальный остаток', 'Конечный остаток', 'Код статьи', 'Код вида расходов', 'Направление расходов.1'], axis=1)
+def razmech(year):
+    return os.path.join("script", f"{year}_razmech.xls")
+
+exp_2020 = pd.read_excel(razmech(2020), skiprows=9, parse_dates=['Дата проведения']).drop(['Unnamed: 0', 'Смета', 'Начальный остаток', 'Конечный остаток', 'Код статьи', 'Код вида расходов', 'Unnamed: 12'], axis=1)
+exp_2021 = pd.read_excel(razmech(2021), skiprows=9, parse_dates=['Дата проведения']).drop(['Unnamed: 0', 'Смета', 'Начальный остаток', 'Конечный остаток', 'Код статьи', 'Код вида расходов', 'Направление расходов.1'], axis=1)
 
 exp_2020_nona = exp_2020.dropna(subset=['Подразделение БР'])
 exp_2020_nona['Месяц'] = exp_2020_nona['Дата проведения'].dt.month
@@ -85,12 +96,6 @@ replacer = {
 
 exp_2020_nona['Наименование вида расходов'] = exp_2020_nona['Наименование вида расходов'].replace(replacer)
 exp_2021_nona['Наименование вида расходов'] = exp_2021_nona['Наименование вида расходов'].replace(replacer)
-
-groups_2 = exp_2020_nona.groupby(['Направление расходов', 'Месяц']).agg({'Отнесено': ['sum', 'count']})
-groups_3 = exp_2021_nona.groupby(['Направление расходов', 'Месяц']).agg({'Отнесено': ['sum', 'count']})
-
-exp_2020_nona = exp_2020_nona.merge(groups_2, on=['Направление расходов', 'Месяц'], how='left').drop('Отнесено', axis=1).drop_duplicates(subset=['Направление расходов', 'Месяц']).rename(columns={('Отнесено', 'sum'): 'Отнесено', ('Отнесено', 'count'): 'Количество'})
-exp_2021_nona = exp_2021_nona.merge(groups_3, on=['Направление расходов', 'Месяц'], how='left').drop('Отнесено', axis=1).drop_duplicates(subset=['Направление расходов', 'Месяц']).rename(columns={('Отнесено', 'sum'): 'Отнесено', ('Отнесено', 'count'): 'Количество'})
 
 exp_2020_nona = exp_2020_nona.merge(temperature_data, on=['Год сметы', 'Месяц', 'Город'], how='left')
 exp_2020_nona.drop('Город', axis=1, inplace=True)
@@ -123,14 +128,9 @@ for df, quartals_wo_name in zip(exp_list, quartals_list):
         df[c] = [quartals_wo_name[c].mean() for _ in range(len(df))]
 
 df = pd.concat(exp_list, ignore_index=True)
+test_df = pd.read_csv('test_payments.csv')
 
-
-print(df.columns)
-print(len(df.columns))
-
-test_df = pd.read_csv('test_df.csv')
-
-combined_df = pd.concat([df.drop('Отнесено', axis=1), test_df], ignore_index=True)
+combined_df = pd.concat([df, test_df], ignore_index=True)
 
 label_encoder = LabelEncoder()
 label_encoder1 = LabelEncoder()
@@ -142,7 +142,7 @@ tfidf = TfidfVectorizer()
 tfidf_encoded = tfidf.fit_transform(combined_df['Направление расходов'])
 
 X_text = tfidf_encoded.toarray()
-X_numeric = combined_df.drop(['Направление расходов'], axis=1).select_dtypes(include=np.number).values
+X_numeric = combined_df.drop(['Отнесено', 'Направление расходов'], axis=1).select_dtypes(include=np.number).values
 
 X = np.concatenate([X_numeric, X_text], axis=1)
 
@@ -171,4 +171,5 @@ model = CatBoostRegressor(border_count=64, depth=8, iterations=1000, l2_leaf_reg
 model.fit(X_train_combined, y)
 
 y_pred = scaler2.inverse_transform(model.predict(X_test_combined).reshape(-1, 1))
-print(y_pred)
+print(np.abs(y_pred))
+
